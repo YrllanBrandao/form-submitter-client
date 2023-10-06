9b543f1c22c205a07b28d145693c19520619c4d5
Yrllan Brandão <yrllanbrandao@outlook.com>
2023-10-06 16:27:59 -0300

    Update main.js

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -4,7 +4,7 @@ const inputOwnerMail = document.getElementById("owner-mail");
 
 form.addEventListener("submit", (e)=>{
     e.preventDefault();
-
+    // 
     const ownerMail = inputOwnerMail.value;
     const formSubmitterDomain = `https://formsubmitter.fast-page.org/submit?target=${ownerMail}`;
     form.action = formSubmitterDomain;
